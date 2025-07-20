64662d6f81c97187a271b180bb48e2acdf1c8817
malathi-francis <115986256+malathi-francis@users.noreply.github.com>
2025-07-20 20:29:23 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import NavBar from './navbar';
+import Navbar from "./Navbar";
 import { Outlet, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
 import axios from 'axios';
